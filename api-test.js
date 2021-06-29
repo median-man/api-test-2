469b611e1dc4636e303814459179b11b4e024907
John Desrosiers <john.desro@gmail.com>
2021-06-29 13:04:54 -0700

    Change to generic fetch tester

--- api-test.js
+++ api-test.js
@@ -1,31 +1,56 @@
-var fetchBtn = document.querySelector("#fetch-btn");
+var requestFormEl = document.querySelector("#req-form");
 var statusEl = document.querySelector("#status");
 var headersEl = document.querySelector("#headers");
 var bodyEl = document.querySelector("#body");
+var urlInput = document.querySelector("#url-input");
+var reqUrlEl = document.querySelector("#req-url");
+var isPending = false;
 
-function fetchMovieData(movieTitle) {
-  var apiKey = "apikey=aeb40d80";
-  var url = new URL("https://www.omdbapi.com");
-  url.search = [apiKey, "t=" + movieTitle].join("&");
-  statusEl.textContent = "fetching ...";
+function renderStatus(text) {
+  statusEl.textContent = text;
+}
+
+function renderRequestedUrl(url) {}
+
+function renderResponseHeaders(response) {
+  let headers = "";
+  for (var pair of response.headers.entries()) {
+    headers += pair[0] + ": " + pair[0] + "\n";
+  }
+  headersEl.textContent = headers;
+}
+
+function renderJson(data) {
+  bodyEl.textContent = JSON.stringify(data, null, 4);
+}
+
+function handleRequestForm(event) {
+  event.preventDefault();
+  if (isPending) {
+    return;
+  }
+  isPending = true;
+  var url = new URL(urlInput.nodeValue.trim());
+  if (!url) {
+    return;
+  }
+  renderStatus("fetching...");
   fetch(url)
     .then(function (response) {
-      console.log(response);
-      statusEl.textContent = response.status;
-      let headers = "";
-      for (var pair of response.headers.entries()) {
-        headers += pair[0] + ": " + pair[0] + "\n";
-      }
-      headersEl.textContent = headers;
-      if (response.ok) {
-        return response.json();
-      }
+      renderStatus(response.status);
+      renderResponseHeaders(response);
+      return response.json();
     })
     .then(function (data) {
-      bodyEl.textContent = JSON.stringify(data, null, 4);
+      renderJson(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      renderJson(error);
+    })
+    .finally(function () {
+      isPending = false;
     });
 }
 
-fetchBtn.addEventListener("click", function () {
-  fetchMovieData("The Matrix");
-});
+requestFormEl.addEventListener("submit", handleRequestForm);
